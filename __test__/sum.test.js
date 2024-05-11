e48b6621af1dd64b03616f3c0c25ca52ed2a752f
Andrew Smithwick <andy.smithwick@gmail.com>
2024-05-10 20:46:00 -0700

    Added a unit test

---
+++ __test__/sum.test.js
@@ -0,0 +1,13 @@
+import { sum } from '../src/scripts/arithmetic';
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2,2)).toBe(4);
+});
+
+test('adds 3 + 3 to equal 6', () => {
+    expect(sum(3,3)).toBe(6);
+  });
