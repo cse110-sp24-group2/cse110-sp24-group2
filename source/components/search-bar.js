a77622cfcceecdf4f508437c1a7a7d0f3f9e2b0b
udsingla <130015308+udsingla@users.noreply.github.com>
2024-05-29 15:29:55 -0700

    Updated search-bar.js to not repeat month colors

--- source/components/search-bar.js
+++ source/components/search-bar.js
@@ -145,35 +145,6 @@ class SearchBar extends HTMLElement {
      * 
      */
     updateButtonColor() {
-        const monthClasses = [
-            "january",
-            "february",
-            "march",
-            "april",
-            "may",
-            "june",
-            "july",
-            "august",
-            "september",
-            "october",
-            "november",
-            "december"
-        ];
-
-        const monthColors = {
-            january: "#c14300",
-            february: "#f29cac",
-            march: "#28a745",
-            april: "#e83e8c",
-            may: "#790258",
-            june: "#154a27",
-            july: "#17a2b8",
-            august: "#d1890c",
-            september: "#dc3545",
-            october: "#d45810",
-            november: "#343a40",
-            december: "#53697d"
-        };
 
         const searchButton = this.shadowRoot.getElementById('search-button');
         searchButton.style.backgroundColor = monthColors.currentMonthClass;
