86b4c0d508967af826cc40d8bbd7dbd778f01df4
Brian <122561802+BrianMorse12604@users.noreply.github.com>
2024-05-29 15:05:06 -0700

    Address some Codacy issues

--- source/Notes/app.js
+++ source/Notes/app.js
@@ -20,24 +20,24 @@ fs.mkdir(dataDir, { recursive: true }, (err) => {
  */
 document.addEventListener('DOMContentLoaded', function() {
     // Retrieve the date from local storage
-    var dateInfo = localStorage.getItem("date");
+    let dateInfo = localStorage.getItem("date");
 
     // Parse the JSON string back into an object
     dateInfo = JSON.parse(dateInfo);
 
     // Create an array of month names
-    var monthNames = ["January", "February", "March", "April", "May", "June",
+    let monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
 
     // Format the date
-    var formattedDate = monthNames[dateInfo.month] + ' ' + dateInfo.day + ' ' + dateInfo.year;
+    let formattedDate = monthNames[dateInfo.month] + ' ' + dateInfo.day + ' ' + dateInfo.year;
 
     // Display the date
     document.getElementById('dateDisplay').textContent = formattedDate;
-    var markdownTextarea = document.getElementById('markdown');
+    let markdownTextarea = document.getElementById('markdown');
     markdownTextarea.addEventListener('input', function() {
-        var markdown = markdownTextarea.value;
-        var html = marked(markdown);
+        let markdown = markdownTextarea.value;
+        let html = marked(markdown);
         document.getElementById('markdownPreview').innerHTML = html;
     });
 
@@ -90,14 +90,14 @@ document.addEventListener('DOMContentLoaded', function() {
  * Opens entry windows for Markdown and To-do list
  */
 function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+        tabcontent[parseInt(i,10)].style.display = "none";
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[parseInt(i,10)].className = tablinks[parseInt(i,10)].className.replace(" active", "");
     }
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
