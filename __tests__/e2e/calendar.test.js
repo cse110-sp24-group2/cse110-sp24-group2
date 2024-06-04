688094827c8138095a2e79e22ba263b41abb55a7
Brian <122561802+BrianMorse12604@users.noreply.github.com>
2024-06-04 16:15:09 -0700

    added pauses

--- __tests__/e2e/calendar.test.js
+++ __tests__/e2e/calendar.test.js
@@ -79,6 +79,7 @@ describe("Navigation and Generation of Calendar", () => {
       let activeDay = activeDays[i];
       let date = await activeDay.getText();
       await activeDay.click();
+      await browser.pause(800);
       // Make sure you are sent to the notes page
       expect(await browser.getUrl()).toHaveUrl(NOTES_URL);
       let dateDisplay = await browser.$("#dateDisplay");
@@ -90,6 +91,7 @@ describe("Navigation and Generation of Calendar", () => {
       // Go back to the calendar page
       let backButton = await browser.$("#backToCalendar");
       await backButton.click();
+      await browser.pause(800);
       // Ensure you are back on the calendar page
       expect(await browser.getUrl()).toHaveUrl(CALENDAR_URL);
     }
@@ -127,6 +129,7 @@ describe("Navigation and Generation of Calendar", () => {
       let activeDay = activeDays[i];
       let date = await activeDay.getText();
       await activeDay.click();
+      await browser.pause(800);
       // Make sure you are sent to the notes page
       expect(await browser.getUrl()).toHaveUrl(NOTES_URL);
       let dateDisplay = await browser.$("#dateDisplay");
@@ -136,6 +139,7 @@ describe("Navigation and Generation of Calendar", () => {
       // Go back to the calendar page
       let backButton = await browser.$("#backToCalendar");
       await backButton.click();
+      await browser.pause(800);
       // Ensure you are back on the calendar page
       expect(await browser.getUrl()).toHaveUrl(CALENDAR_URL);
       // Get back to the previous month
@@ -175,6 +179,7 @@ describe("Navigation and Generation of Calendar", () => {
       let activeDay = activeDays[i];
       let date = await activeDay.getText();
       await activeDay.click();
+      await browser.pause(800);
       // Make sure you are sent to the notes page
       expect(await browser.getUrl()).toHaveUrl(NOTES_URL);
       let dateDisplay = await browser.$("#dateDisplay");
@@ -184,6 +189,7 @@ describe("Navigation and Generation of Calendar", () => {
       // Go back to the calendar page
       let backButton = await browser.$("#backToCalendar");
       await backButton.click();
+      await browser.pause(800);
       // Ensure you are back on the calendar page
       expect(await browser.getUrl()).toHaveUrl(CALENDAR_URL);
       // Get back to the next month
@@ -225,6 +231,7 @@ describe("Navigation and Generation of Calendar", () => {
       let activeDay = activeDays[i];
       let date = await activeDay.getText();
       await activeDay.click();
+      await browser.pause(800);
       // Make sure you are sent to the notes page
       expect(await browser.getUrl()).toHaveUrl(NOTES_URL);
       let dateDisplay = await browser.$("#dateDisplay");
@@ -234,6 +241,7 @@ describe("Navigation and Generation of Calendar", () => {
       // Go back to the calendar page
       let backButton = await browser.$("#backToCalendar");
       await backButton.click();
+      await browser.pause(800);
       // Ensure you are back on the calendar page
       expect(await browser.getUrl()).toHaveUrl(CALENDAR_URL);
       // Get back to the next year
