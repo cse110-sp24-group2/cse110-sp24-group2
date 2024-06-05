8cce8ce055eabf40657bd3932640a2b6e5bb3702
Anya Chernova <achernova@ucsd.edu>
2024-06-04 17:59:13 -0700

    fixed margins of todo list and calendar

--- source/components/TodoList.js
+++ source/components/TodoList.js
@@ -13,9 +13,10 @@ class TodoList extends HTMLElement {
             width: auto;
             max-width: 450px;
             height: auto;
-            padding: 30px;
+            padding: 20px;
             background-color: rgba(255 255 255 / 80%); 
             margin-top: 25px;
+            margin-right: 20px;
             box-shadow: 0 6px 12px rgba(0 0 0 / 15%);  
             color: #044c4d;
         }
