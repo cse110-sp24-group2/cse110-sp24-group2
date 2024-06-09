a948712d810a527fc43cc047f4c3f593529458fb
Brian <122561802+BrianMorse12604@users.noreply.github.com>
2024-06-08 19:18:48 -0700

    Adding pauses to searchPage tests

--- __tests__/e2e/searchPage.test.js
+++ __tests__/e2e/searchPage.test.js
@@ -1,7 +1,6 @@
 const { browser } = require("@wdio/globals");
 const path = require("path");
 
-
 //Declare Variables to be used throughout testing and set before each test
 let CALENDAR_URL;
 let SEARCH_URL;
@@ -14,8 +13,18 @@ const currentYear = new Date().getFullYear();
 
 // Array of month names
 const monthNames = [
-  "January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 describe("Navigation and Generation of SearchPage", () => {
@@ -29,10 +38,7 @@ describe("Navigation and Generation of SearchPage", () => {
         CALENDAR_URL
       ).href;
 
-      NOTES_URL = new URL(
-        "../../source/Notes/index.html",
-        CALENDAR_URL
-      ).href;
+      NOTES_URL = new URL("../../source/Notes/index.html", CALENDAR_URL).href;
     }
 
     // Click on the search bar
@@ -46,7 +52,7 @@ describe("Navigation and Generation of SearchPage", () => {
     // Add a brief pause to ensure page load
     await browser.pause(800);
   });
-  
+
   // Test if clicking on the "Back to Calendar" button takes you to the calendar page
   it("Verify Back to Calendar Navigation", async () => {
     // Click on the "Back to Calendar" button
@@ -80,40 +86,39 @@ describe("Navigation and Generation of SearchPage", () => {
     const enteredText = await searchBar.getValue();
     expect(enteredText).toBe(searchText);
   }, 10000);
-  
+
   // Added label should show up on searchPage
   it("Add Label to Notes Page and Verify on Search Page", async () => {
     // First you are on the calendar page
     await browser.url(CALENDAR_URL);
+    await browser.pause(800);
     // Click on the current day. take you to notes page
-    const currentDay = await $('.current-day');
-    await browser.pause(3000);
+    const currentDay = await $(".current-day");
     await currentDay.click();
-    
+    await browser.pause(800);
 
     // Now, we should be on the notes page
     // Let's add a label
     const labelName = "test-label-final-test";
     const labelColor = "#1E90FF";
-  
+
     // Enter the label name
-    const labelNameInput = await $('#label-name');
+    const labelNameInput = await $("#label-name");
     await labelNameInput.setValue(labelName);
-  
+
     // Enter the label color
-    const labelColorInput = await $('#label-color');
+    const labelColorInput = await $("#label-color");
     await labelColorInput.setValue(labelColor);
-  
+
     // Click the add label button
-    const addLabelButton = await $('#add-label-button');
+    const addLabelButton = await $("#add-label-button");
     await addLabelButton.click();
 
-    
     // Go back to calendar
     const backToCalendarButton = await browser.$("#backToCalendar");
     await backToCalendarButton.click();
-    
-    
+    await browser.pause(800);
+
     // Click on the search bar
     await browser.execute(() => {
       document.querySelector("#search-bar").click();
@@ -121,38 +126,37 @@ describe("Navigation and Generation of SearchPage", () => {
 
     // Navigate to the search page
     await browser.url(SEARCH_URL);
-    await browser.pause(3000);
-
+    await browser.pause(800);
 
     // Check that one of the label is the one I created.
     const labelsContainer = await browser.$("#labels-container");
-    const childLabels = await labelsContainer.$$('*');
+    const childLabels = await labelsContainer.$$("*");
 
     let hasChildWithText = false;
 
     for (const childLabel of childLabels) {
       const text = await childLabel.getText();
-      if (text.trim() === 'test-label-final-test') {
+      if (text.trim() === "test-label-final-test") {
         hasChildWithText = true;
         break;
       }
     }
-    expect(hasChildWithText).toBe(true);    
+    expect(hasChildWithText).toBe(true);
   });
 
   // Check if the label created on a particular date has the correct data
   it("Check if labels show up with correct dates", async () => {
     // Click on one of the labels
     const labelsContainer = await browser.$("#labels-container");
-    const childLabels = await labelsContainer.$$('*');
+    const childLabels = await labelsContainer.$$("*");
 
     // Loop through the labels to find the one we want
     for (const childLabel of childLabels) {
       const text = await childLabel.getText();
-      if (text.trim() === 'test-label-final-test') {
+      if (text.trim() === "test-label-final-test") {
         await childLabel.click();
         // Check if the date that we see is the same as the current date
-        const dateButton = await browser.$('.date-button');
+        const dateButton = await browser.$(".date-button");
         const displayedDate = await dateButton.getText();
 
         // Check if the date is correct
@@ -166,23 +170,23 @@ describe("Navigation and Generation of SearchPage", () => {
   it("Check if date can be clicked and leads to the right notes", async () => {
     // Click on one of the labels
     const labelsContainer = await browser.$("#labels-container");
-    const childLabels = await labelsContainer.$$('*');
+    const childLabels = await labelsContainer.$$("*");
 
     // Loop through the labels to find the one we want
     for (const childLabel of childLabels) {
       const text = await childLabel.getText();
-      if (text.trim() === 'test-label-final-test') {
+      if (text.trim() === "test-label-final-test") {
         await childLabel.click();
 
         // Click the date button
-        const dateButton = await browser.$('.date-button');
-        await dateButton.click()
+        const dateButton = await browser.$(".date-button");
+        await dateButton.click();
 
         // Check if notes are loaded
         expect(await browser.getUrl()).toBe(NOTES_URL);
 
         // Check if these are notes for correct day
-        const notesDate = await (await browser.$('h1')).getText();
+        const notesDate = await (await browser.$("h1")).getText();
 
         const formattedCurrentDate = `${monthNames[currentMonth]} ${currentDate}, ${currentYear}`;
         expect(notesDate).toBe(formattedCurrentDate);
@@ -194,17 +198,17 @@ describe("Navigation and Generation of SearchPage", () => {
       }
     }
   });
-  
+
   it("Check that non-existent label does not show up", async () => {
     // Get all labels
     const labelsContainer = await browser.$("#labels-container");
-    const childLabels = await labelsContainer.$$('*');
-  
+    const childLabels = await labelsContainer.$$("*");
+
     // Check that a non-existent label does not show up
     let found = false;
     for (const childLabel of childLabels) {
       const text = await childLabel.getText();
-      if (text.trim() !== 'test-label-final-test') {
+      if (text.trim() !== "test-label-final-test") {
         found = true;
         break;
       }
@@ -212,5 +216,3 @@ describe("Navigation and Generation of SearchPage", () => {
     expect(found).toBe(false);
   });
 });
-
-
